ee99663e1f6a2358a64ea5766d342879e672d310
Mark Anthony <touya1313@hotmail.com>
2024-04-08 23:50:08 -0600

    redoing my server.js

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
+// Sets up my Express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up Sequelize 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3003;
+// Redoing it above this. 
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
