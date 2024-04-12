f02fdcdb47bfb52ffa85ee5c476391d5d3179d88
Mark Anthony <touya1313@hotmail.com>
2024-04-12 00:41:53 -0600

    formatted the doc

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,34 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Route to display a list of posts on the homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [
         {
-          model: User
+          model: User,
         },
-      ]
+      ],
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('all-posts', { posts });
+    res.render("all-posts", { posts });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // Route to display the login page
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-    res.render('login');
+  res.render("login");
 });
 
 // Redirects to another route
@@ -42,22 +42,22 @@ router.get("/signup", (req, res) => {
 });
 
 // Route to display an individual post
-router.get('/post/:id', (req, res) => {
+router.get("/post/:id", (req, res) => {
   Post.findByPk(req.params.id, {
-      include: [
-          User,
-        {
-          model: Comment,
-          include: [User],
-          },
-      ],
-    })
+    include: [
+      User,
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  })
 
     .then((dbPostData) => {
       if (dbPostData) {
         const post = dbPostData.get({ plain: true });
 
-        res.render('single-post', { post });
+        res.render("single-post", { post });
       } else {
         res.status(404).end();
       }
