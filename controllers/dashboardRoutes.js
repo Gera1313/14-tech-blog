1166e6c07e5a8cf279f5d3d3b4b7e2ec8f313d8a
Mark Anthony <touya1313@hotmail.com>
2024-04-12 20:20:04 -0600

    will finish after dinner

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -7,4 +7,9 @@ router.get("/", withAuth, (req, res) => {
     Post.findAll({
         where: { userId: req.session.userId }
     })
+    .then(dbPostData => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+        res.render
+    })
 })
