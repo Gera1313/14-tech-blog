db8052e9dbb0adac766a58e7a02a6fe641c6aeb2
Mark Anthony <touya1313@hotmail.com>
2024-04-12 20:09:26 -0600

    did index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 // Import and use the routes defined in other files
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes'); // Do I need .js?
+const apiRoutes = require('./api/');
+const dashboardRoutes = require('./dashboardRoutes'); // Do I need .js? 
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
