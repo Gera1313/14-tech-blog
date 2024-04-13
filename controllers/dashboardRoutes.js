f976199539ff19ce0567bcc9ce311e7f01b4049d
Mark Anthony <touya1313@hotmail.com>
2024-04-12 20:18:19 -0600

    router.get

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,3 +3,8 @@ const { Post } = require("../models/");
 const withAuth = require("../utils/auth");
 
 // Get posts by user when user logs in
+router.get("/", withAuth, (req, res) => {
+    Post.findAll({
+        where: { userId: req.session.userId }
+    })
+})
