ece7014f34b5496708af3f96b85a2d7087f10cd4
Mark Anthony <touya1313@hotmail.com>
2024-04-15 17:23:38 -0600

    finished commentroutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,3 +3,14 @@ const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 //POST comments omtp existing posts
+router.post("/", withAuth, (req, res) => {
+    Comment.create({ ...req.body, userId: req.session.userId })
+    .then(newComment => {
+        res.json(newComment);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
