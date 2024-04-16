0bf44c8386bf8d1d87501cc918e2f06c750a76f4
Mark Anthony <touya1313@hotmail.com>
2024-04-16 00:20:23 -0600

    end here

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.post
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then //hereeeeeeee
+})
