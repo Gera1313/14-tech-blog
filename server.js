e07a248adc78299156a4c1385455c869b43ef591
Mark Anthony <touya1313@hotmail.com>
2024-04-17 15:29:08 -0600

    found issue and can finally connect to my server. New problem, have to create login.handlebars

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 // Sets up my session with cookies
 const sess = {
   secret: 'Super secret secret',
