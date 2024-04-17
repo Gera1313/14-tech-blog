1efb995c40ce23877a3937e3f46ee9ad4402fc25
Mark Anthony <touya1313@hotmail.com>
2024-04-16 23:49:48 -0600

    left off .catch err

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,4 +15,8 @@ router.post("/", (req, res) => {
             res.json(dbUserData);
         });
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 })
