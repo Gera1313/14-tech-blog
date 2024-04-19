cab6ee6aa927ea671bdc719c06605c7c19364809
Mark Anthony <touya1313@hotmail.com>
2024-04-19 12:40:07 -0600

    started user in models

--- models/user.js
+++ models/user.js
@@ -10,28 +10,23 @@ class User extends Model {
 
 User.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
       },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+      username: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [4]
+          }
+      }
   },
   {
     sequelize,
